4b36400531237ef51fcb0c6afd14dfe871224590
Lefty <lefty@yahoo.com>
2020-09-15 20:25:27 +0200

    [bugfix] fix order of output according to rank number.

--- helloworld.py
+++ helloworld.py
@@ -4,4 +4,9 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 nprocs = comm.Get_size()
 
-print("Hello World! I'm rank ", rank," out of ",nprocs,".")
+comm.barrier()
+for i in range(0,4):
+    if(i == rank):
+        print("Hello World! I'm rank ", rank," out of ",nprocs,".",flush=True)
+    comm.barrier()
+
