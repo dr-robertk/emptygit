2b4de8ef0797fbe1a643ac42663a6cd420952674
Dusty Boots, PhD <dusty@boots.org>
2020-09-15 20:09:25 +0200

    [feature] Added rank and size output for parallel runs.

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,7 @@
-print('Hello World!')
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+nprocs = comm.Get_size()
+
+print("Hello World! I'm rank ", rank," out of ",nprocs,".")
