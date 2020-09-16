8bb8e814b445dd0d4467273ed356796895fdc3cf
Lefty <lefty@yahoo.com>
2020-09-16 13:26:47 +0200

    [feature] Added computation of maximal rank number.

--- helloworld.py
+++ helloworld.py
@@ -9,4 +9,7 @@ for i in range(0,4):
     if(i == rank):
         print("Hello World! I'm rank ", rank," out of ",nprocs,".",flush=True)
     comm.barrier()
+maxRank = comm.allreduce( rank, op=MPI.MAX )
+if( rank == 0 ):
+    print('Max rank is ',maxRank)
 
